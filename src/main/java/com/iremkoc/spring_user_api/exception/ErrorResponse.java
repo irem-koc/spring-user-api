1ec2fdd6e7c336f81238f7851eb841f663878484
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-04-27 20:25:30 +0300

    Add ErrorResponse class for handling exceptions

---
+++ src/main/java/com/iremkoc/spring_user_api/exception/ErrorResponse.java
@@ -0,0 +1,10 @@
+package com.iremkoc.spring_user_api.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+}
