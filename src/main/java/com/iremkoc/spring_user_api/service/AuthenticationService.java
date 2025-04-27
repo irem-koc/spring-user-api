00750001836d7803e59310615c70d1467a1ee2a2
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-04-27 20:24:00 +0300

    Update AuthenticationService interface to use LoginResponse

--- src/main/java/com/iremkoc/spring_user_api/service/AuthenticationService.java
+++ src/main/java/com/iremkoc/spring_user_api/service/AuthenticationService.java
@@ -2,10 +2,11 @@ package com.iremkoc.spring_user_api.service;
 
 import com.iremkoc.spring_user_api.model.AuthenticationResponse;
 import com.iremkoc.spring_user_api.model.LoginRequest;
+import com.iremkoc.spring_user_api.model.LoginResponse;
 import com.iremkoc.spring_user_api.model.RegisterRequest;
 
 public interface AuthenticationService {
-    AuthenticationResponse login(LoginRequest request);
+    LoginResponse login(LoginRequest request);
 
     AuthenticationResponse register(RegisterRequest request);
 }
