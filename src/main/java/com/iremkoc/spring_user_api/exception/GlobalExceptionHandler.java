03961c0ba02e35dc90f9ec02ad5ecda18d82c73c
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-04-27 20:25:35 +0300

    Add GlobalExceptionHandler for handling exceptions

---
+++ src/main/java/com/iremkoc/spring_user_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.iremkoc.spring_user_api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse("Bilinmeyen bir hata oluştu: " + ex.getMessage()));
+    }
+
+    @ExceptionHandler(value = { UsernameNotFoundException.class })
+    public ResponseEntity<Object> handleUserNotFound(UsernameNotFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponse("Kullanıcı bulunamadı: " + e.getMessage()));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleBadCredentials(BadCredentialsException ex) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponse("E-posta veya şifre hatalı: " + ex.getMessage()));
+    }
+}
