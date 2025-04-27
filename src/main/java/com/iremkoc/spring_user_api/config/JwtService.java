02396c0e3b75d59aa009fc8fec37b5cfd4ab59e8
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-04-27 20:23:22 +0300

    Refactor JwtService class to use a SecretKey instead of a String for the secret key

--- src/main/java/com/iremkoc/spring_user_api/config/JwtService.java
+++ src/main/java/com/iremkoc/spring_user_api/config/JwtService.java
@@ -1,62 +1,73 @@
 package com.iremkoc.spring_user_api.config;
 
-import java.security.Key;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Date;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.security.core.userdetails.UserDetails;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 
 public class JwtService {
-    private static final String SECRET_KEY = "4776781E866ACBD82FE4F86543D6A";
-    private static final Integer EXPIRATION_TIME = 1000 * 60 * 24 * 7;
 
-    public String extractUsername(String jwt) {
-        return extractClaim(jwt, Claims::getSubject);
-    }
+    private static final long EXPIRATION_TIME = 1000 * 60 * 24 * 7;
+    private final SecretKey Key;
 
-    public String generateToken(UserDetails userDetails) {
-        return generateToken(new HashMap<>(), userDetails);
+    public JwtService() {
+        String secretString = "ksxBgo8Lfljf8Q4dhBYWjfeSYTRiLck16yk8JZh74g8=";
+        if (secretString == null || secretString.isEmpty()) {
+            throw new IllegalStateException("Çevresel değişken 'TOKEN_KEY' bulunamadı.");
+        }
+        byte[] secretBytes = Base64.getDecoder().decode(secretString.getBytes(StandardCharsets.UTF_8));
+        this.Key = new SecretKeySpec(secretBytes, "HmacSHA256");
     }
 
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
-        return Jwts.builder()
-                .subject((userDetails.getUsername()))
+        return Jwts
+                .builder()
+                .subject(userDetails.getUsername())
+                .signWith(Key)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date((new Date(System.currentTimeMillis())).getTime() + EXPIRATION_TIME))
-                .signWith(getSignInKey())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .claims(extraClaims)
                 .compact();
     }
 
-    public <T> T extractClaim(String jwt, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(jwt);
-        return claimsResolver.apply(claims);
+    public String extractUsername(String token) {
+        return extractClaims(token, Claims::getSubject);
     }
 
-    private Claims extractAllClaims(String jwt) {
-        return Jwts.parser().setSigningKey(getSignInKey()).build().parseClaimsJws(jwt).getBody();
+    private <T> T extractClaims(String token, Function<Claims, T> claimsTFunction) {
+        return claimsTFunction.apply(Jwts.parser().verifyWith(Key).build().parseSignedClaims(token).getPayload());
     }
 
-    private Key getSignInKey() {
-        byte[] keyBytes = SECRET_KEY.getBytes();
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
-
-    public boolean isTokenValid(String token, UserDetails userDetails) {
+    public boolean validateToken(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
     private boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
+        return extractClaims(token, Claims::getExpiration).before(new Date());
     }
 
-    private Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
+    private final Set<String> blacklistedTokens = new HashSet<>();
+
+    public void invalidateToken(String token) {
+        blacklistedTokens.add(token);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername())
+                && !isTokenExpired(token)
+                && !blacklistedTokens.contains(token);
     }
 }
